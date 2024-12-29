ad395c0d89a574b8e4cd402c468693d0aa5bcf6c
oluwasegunamao10 <shegxyamao@gmail.com>
2024-12-29 21:49:05 +0100

    Added a new NavBar component and installed react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
-    <div>   <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1></div>
-  )
-}
+    <div>
+      <Navbar />
+    </div>
+  );
+};
 
-export default App
+export default App;
